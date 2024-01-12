1344b1d2b0564b04f185a1d2363a6d43b27fb1d3
SaaighTaimur <146673423+SaaighTaimur@users.noreply.github.com>
2024-01-12 10:55:08 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,45 @@
 import streamlit as st
+import json
+from streamlit_lottie import st_lottie
 
-st.title('🎈 App Name')
+# Set page configuration
+st.set_page_config(
+    page_title="FinanceFusion",
+    page_icon="💸"
+)
 
-st.write('Hello world!')
+# Load Lottie file
+def load_lottie(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+    
+working_lottie = load_lottie("lotties/working.json")
+
+
+
+# Header
+st.image("banner_with_slogan.png")
+
+# Introduction
+st.write("""
+Welcome to FinanceFusion, your all-in-one financial companion! Explore the following features:
+
+- 💰 **Budget Tool:** Manage your finances effectively.
+- 📈 **Stock Tracker:** Keep an eye on your favorite stocks.
+- 📊 **Stock Comparison:** Compare different stocks at a glance.
+
+We appreciate your feedback! Please use the 📢 feedback form available to share your thoughts.
+""")
+
+
+# Show lottie
+st_lottie(
+    working_lottie,
+    speed=1,
+    reverse=False,
+    loop=True,
+    quality="low",
+)
+
+# Sidebar Message
+st.sidebar.success("Select a page above")
